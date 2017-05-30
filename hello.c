84a4f3fa5faef0ab20d02a1a10d753172f036cd2
Yury Dementsov <cybdyn@rambler.ru>
2017-05-30 15:28:23 +0300

    Using dlopen,...

--- hello.c
+++ hello.c
@@ -1,14 +1,37 @@
 
 #include <stdio.h>
+#include <dlfcn.h>
 
-const char * get_hello(void);
 const char * get_bye(void);
 
 int main(void)
 {
+   void *lib_handle;
+   const char * (*fn)(void);
+   char *error;
 
-    printf("%s\n", get_hello());
-    printf("%s\n", get_bye());
+
+   lib_handle = dlopen("/opt/lib/libhello.so", RTLD_LAZY);
+
+   if (!lib_handle)
+   {
+      fprintf(stderr, "%s\n", dlerror());
+      exit(1);
+   }
+
+   fn = dlsym(lib_handle, "get_hello");
+
+   if ((error = dlerror()) != NULL)
+   {
+      fprintf(stderr, "%s\n", error);
+      exit(1);
+   }
+
+   printf("%s\n", fn() );
+
+   dlclose(lib_handle);
+
+   printf("%s\n", get_bye());
 
    return 0;
 }
